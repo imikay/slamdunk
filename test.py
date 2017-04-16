e11db5b758775c49864effe31805ed41e351462c
imikay <imikay@gmail.com>
2017-04-16 19:45:46 +0800

    Change IR to a new one

--- test.py
+++ test.py
@@ -3,18 +3,19 @@ import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(36, GPIO.IN)
 
 count = 0;
 
 try:
     while True:
-              i=GPIO.input(12)                       
+              i = GPIO.input(36)                       
+              print "Status: ", i
 
-              if i == 0:                              
+              if i == 1:                              
                    count = count + 3
                    print "Goal! Total score ", count
-                   time.sleep(0.8)
+              time.sleep(0.1)
 except KeyboardInterrupt:
     GPIO.cleanup()
 GPIO.cleanup()
